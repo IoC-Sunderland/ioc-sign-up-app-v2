20de118bbacff99247bea00b4a7ce50725c1ce00
IoC-Sunderland <gavin.mcclary@sunderland.ac.uk>
2020-12-02 12:56:05 +0000

    Changed db settings to postgres and removed id field from User model as not required by postgres db, it will auto-increment itself

--- sign_up_app/settings.py
+++ sign_up_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'sign_up_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sign_up_app_db',
+        'USER': 'postgres',
+        'PASSWORD': 'dU9dQ8bZ',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- signup/models.py
+++ signup/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Users(models.Model):
-    id = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=200, unique=True, null=False)
     email = models.EmailField(max_length=254, unique=True, null=False)
     password = models.CharField(max_length=128)
